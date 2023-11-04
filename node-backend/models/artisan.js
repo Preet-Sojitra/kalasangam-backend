51f7a242432a5a222e9446c67570c01c94e6b923
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-11-04 10:05:12 +0530

    fix: minor changes

--- node-backend/models/artisan.js
+++ node-backend/models/artisan.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose')
 const artisanSchema = new mongoose.Schema({
     name:String,
     age: Number,
-    gender:{
-        type:String,
-        enum: ["male", "female"]
-    },
     address: String,
     mobile: String,
     password:String,
