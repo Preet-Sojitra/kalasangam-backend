57fd0c627fd32ad8e45b79d730e71903ff13f41c
Kunal Kumar Sahoo <kunal.sahoo2003@gmail.com>
2023-11-04 10:31:17 +0530

    boilerplate

---
+++ flask-backend/init.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+
+load_dotenv()
+URI = os.getenv("URI")
+
+app  = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
