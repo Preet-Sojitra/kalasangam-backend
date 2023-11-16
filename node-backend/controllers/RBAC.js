78ae3d63c316d82c68f82a4786b05af8ca78c114
Preet Sojitra <preet.dev373@gmail.com>
2023-11-17 02:20:38 +0530

    fix: add cors, bcrypt password not matching issue

--- node-backend/controllers/RBAC.js
+++ node-backend/controllers/RBAC.js
@@ -1,118 +1,127 @@
-const Artisan = require("../models/artisan");
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const Artisan = require("../models/artisan")
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 // const translate = require("../utils/translate");
 
-exports.artisanLogin = async (req, res)=>{
-    const {mobile, password} = req.body;
-    try{
-        let artisan = await Artisan.findOne({mobile});
-        if(!artisan)
-            return res.status(403).json({message: "User already exists"});
-        const isMatch = bcrypt.compare(password, artisan.password);
-        if(!isMatch)
-            return res.status(403).json({message: "Invalid credentials"});
-        const token = jwt.sign({
-            id: artisan._id
-        }, process.env.JWT_SECRET);
-        res.cookie("accessToken", token);
-        return res.status(200).json({
-            message: "Login successful",
-            accessToken: token
-        });
-    }
-    catch(error){
-        return res.status(500).json({message: error.message});
-    }
-
+exports.artisanLogin = async (req, res) => {
+  const { mobile, password } = req.body
+  try {
+    let artisan = await Artisan.findOne({ mobile })
+    if (!artisan)
+      return res.status(403).json({ message: "User does not exists" })
+    const isMatch = await bcrypt.compare(password, artisan.password) // Here await is necessary
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" })
+    const token = jwt.sign(
+      {
+        id: artisan._id,
+      },
+      process.env.JWT_SECRET
+    )
+    res.cookie("accessToken", token)
+    return res.status(200).json({
+      message: "Login successful",
+      accessToken: token,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
-
-exports.userLogin = async (req, res)=>{
-    const {mobile, password} = req.body;
-    try{
-        let user = await User.findOne({mobile});
-        if(!user)
-            return res.status(403).json({message: "User already exists"});
-        const isMatch = bcrypt.compare(password, user.password);
-        if(!isMatch)
-            return res.status(403).json({message: "Invalid credentials"});
-        const token = jwt.sign({
-            id: user._id
-        }, process.env.JWT_SECRET);
-        // res.cookie.accessToken = token;
-        res.cookie("accessToken", token);
-        return res.status(200).json({
-            message: "Login successful",
-            accessToken: token
-        });
-    }
-    catch(error){
-        return res.status(500).json({message: error.message});
-    }
+exports.userLogin = async (req, res) => {
+  const { mobile, password } = req.body
+  try {
+    let user = await User.findOne({ mobile })
+    if (!user) return res.status(403).json({ message: "User already exists" })
+    const isMatch = bcrypt.compare(password, user.password)
+    if (!isMatch)
+      return res.status(403).json({ message: "Invalid credentials" })
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET
+    )
+    // res.cookie.accessToken = token;
+    res.cookie("accessToken", token)
+    return res.status(200).json({
+      message: "Login successful",
+      accessToken: token,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
-exports.artisanRegister = async (req, res)=>{
-    const {name, age, address, mobile, password} = req.body;
-    try{
-        let artisan = await Artisan.findOne({mobile});
-        if(artisan)
-            return res.status(403).json({message: "User already exists"});
-        // name = translate(name);
-        // address = translate(address);
-        artisan = new Artisan({
-            name,
-            age,
-            address,
-            mobile,
-            password
-        });
-        const salt = await bcrypt.genSalt(10);
-        artisan.password = await bcrypt.hash(password, salt);
-        await artisan.save();
-        const token = jwt.sign({
-            id: artisan._id
-        }, process.env.JWT_SECRET);
-        // res.cookie.accessToken = token;
-        res.cookie("accessToken", token);
-        return res.status(200).json({
-            message: "Registration successful",
-            accessToken: token
-        });
-    }
-    catch(error){
-        return res.status(500).json({message: error.message});
-    }
+exports.artisanRegister = async (req, res) => {
+  const { name, age, address, mobile, password } = req.body
+  try {
+    let artisan = await Artisan.findOne({ mobile })
+    if (artisan) return res.status(403).json({ message: "User already exists" })
+    // name = translate(name);
+    // address = translate(address);
+    artisan = new Artisan({
+      name,
+      age,
+      address,
+      mobile,
+      password,
+    })
+    const salt = await bcrypt.genSalt(10)
+    artisan.password = await bcrypt.hash(password, salt)
+    await artisan.save()
+    const token = jwt.sign(
+      {
+        id: artisan._id,
+      },
+      process.env.JWT_SECRET
+    )
+    // res.cookie.accessToken = token;
+    res.cookie("accessToken", token)
+    return res.status(200).json({
+      message: "Registration successful",
+      accessToken: token,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
 exports.userRegister = async (req, res) => {
-    const {name, mobile, password} = req.body;
-    if(name === undefined || mobile === undefined || mobile.length === 0 || password === undefined || password.length === 0)
-        return res.status(400).json({message: "Bad request"});
-    try{
-        let user = await User.findOne({mobile});
-        if(user)
-            return res.status(403).json({message: "User already exists"});
-        // name = translate(name);
-        user = new User({
-            name,
-            mobile,
-            password
-        });
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-        await user.save();
-        const token = jwt.sign({
-            id: user._id
-        }, process.env.JWT_SECRET);
-        // res.cookie.accessToken = token;
-        res.cookie("accessToken", token);
-        return res.status(201).json({
-            accessToken: token
-        });
-    }
-    catch(error) {
-        return res.status(500).json({message: error.message});
-    }
+  const { name, mobile, password } = req.body
+  if (
+    name === undefined ||
+    mobile === undefined ||
+    mobile.length === 0 ||
+    password === undefined ||
+    password.length === 0
+  )
+    return res.status(400).json({ message: "Bad request" })
+  try {
+    let user = await User.findOne({ mobile })
+    if (user) return res.status(403).json({ message: "User already exists" })
+    // name = translate(name);
+    user = new User({
+      name,
+      mobile,
+      password,
+    })
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt)
+    await user.save()
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET
+    )
+    // res.cookie.accessToken = token;
+    res.cookie("accessToken", token)
+    return res.status(201).json({
+      accessToken: token,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
--- node-backend/index.js
+++ node-backend/index.js
@@ -1,42 +1,49 @@
-const express = require('express')
-const morgan = require('morgan')
-const fileUpload = require('express-fileupload')
-const mongoose = require('mongoose')
-const productRouter = require('./routes/product')
-const analyticsRouter = require('./routes/analytics')
-const {sequelize} = require('./db/db')
-const RBACRouter = require('./routes/RBAC')
-require('dotenv').config()
+const express = require("express")
+const morgan = require("morgan")
+const fileUpload = require("express-fileupload")
+const mongoose = require("mongoose")
+const productRouter = require("./routes/product")
+const analyticsRouter = require("./routes/analytics")
+const { sequelize } = require("./db/db")
+const RBACRouter = require("./routes/RBAC")
+require("dotenv").config()
+cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-app.use(morgan('tiny'))
-app.use(fileUpload({
+app.use(morgan("tiny"))
+app.use(
+  fileUpload({
     useTempFiles: true,
-    tempFileDir: '/tmp/',
-}))
+    tempFileDir: "/tmp/",
+  })
+)
 
-app.use("/api/v1/product",productRouter)
+app.use("/api/v1/product", productRouter)
 app.use("/api/v1", RBACRouter)
-app.use("/api/v1",analyticsRouter)
+app.use("/api/v1", analyticsRouter)
 
+// Test route
+app.get("/", (req, res) => {
+  res.send("Testing, 123")
+})
 
-mongoose.connect(process.env.URI)
-    .then(() => {
-        console.log("DB connected");
-        const Port = 3000 || process.env.PORT;
-        app.listen(Port, () => {
-            console.log(`Server started on port ${process.env.PORT}`);
-        })
-        // sequelize.authenticate().then(() => {
-        //     console.log("Connected to RDS");
-        // })
-        .catch(err => {
-            console.log(err);
-        })
-    }).catch((err)=>{
-        console.log(err);
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log("DB connected")
+    const Port = 3000 || process.env.PORT
+    app.listen(Port, () => {
+      console.log(`Server started on port ${process.env.PORT}`)
     })
+    // sequelize.authenticate().then(() => {
+    //     console.log("Connected to RDS");
+    // })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
