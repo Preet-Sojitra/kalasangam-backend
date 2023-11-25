2377384945c2723b8124b1a71d5ad5dc77a5693b
Preet Sojitra <preet.dev373@gmail.com>
2023-11-25 09:49:10 +0530

    fix: add to cart controller

--- node-backend/controllers/cart.js
+++ node-backend/controllers/cart.js
@@ -1,43 +1,51 @@
-const Cart = require('../models/cart')
+const Cart = require("../models/cart")
 
 // send customer id in params
-exports.getCart = async(req,res) => {
-    try {
-        const id = req.params.id 
-        const cart = await Cart.find({customer: id}).populate("product")
-        if(!cart){
-            return res.status(400).json("cart not found")
-        }
-        return res.status(200).json(cart)
-    } catch (error) {
-        return res.status(500).json(error.message)
+exports.getCart = async (req, res) => {
+  try {
+    const id = req.params.id
+    const cart = await Cart.find({ customer: id }).populate("product")
+    if (!cart) {
+      return res.status(400).json("cart not found")
     }
+    return res.status(200).json(cart)
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
 
-exports.addProductToCart = async(req,res) => {
-    try {
-        const {customer,productId} = req.body 
-        const cart = await Cart.findOne({customer})
-        if(!cart){
-            return res.status(400).json("cart not found")
-        }
-        await cart.product.push(productId)
-        return res.status(200).json("product added")
-    } catch (error) {
-        return res.status(500).json(error.message)
+exports.addProductToCart = async (req, res) => {
+  try {
+    const { customer, productId } = req.body
+    console.log(req.body)
+    const cart = await Cart.findOne({ customer })
+    if (!cart) {
+      const newCart = await Cart.create({
+        customer,
+      })
+      await newCart.product.push(productId)
+      await newCart.save()
+      console.log(newCart)
+      return res.status(200).json("product added")
     }
+    await cart.product.push(productId)
+    await cart.save()
+    return res.status(200).json("product added")
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
 
-exports.removeProductFromCart = async(req,res) => {
-    try {
-        const {customer,productId} = req.body 
-        const cart = await Cart.findOne({customer})
-        if(!cart){
-            return res.status(400).json("cart not found")
-        }
-        await cart.product.pop(productId)
-        return res.status(200).json("product added")
-    } catch (error) {
-        return res.status(500).json(error.message)
+exports.removeProductFromCart = async (req, res) => {
+  try {
+    const { customer, productId } = req.body
+    const cart = await Cart.findOne({ customer })
+    if (!cart) {
+      return res.status(400).json("cart not found")
     }
+    await cart.product.pop(productId)
+    return res.status(200).json("product added")
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
